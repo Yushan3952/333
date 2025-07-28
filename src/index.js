4577a4d7b57511c244a2859f01548c9d12ee01a9
Yushan3952 <sow112021012@gmail.com>
2025-07-28 20:10:18 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import './index.css';
+import AdminPanel from './AdminPanel';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AdminPanel />
   </React.StrictMode>
 );
