7ecc3cdf4296bb1875c899eece86f3e6e0869d7f
Yushan3952 <sow112021012@gmail.com>
2025-11-27 21:08:38 +0800

    Update firebase.js

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,13 +1,11 @@
 import admin from 'firebase-admin';
-import dotenv from 'dotenv';
-dotenv.config();
 
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      projectId: process.env.VITE_FIREBASE_PROJECT_ID,
-      clientEmail: process.env.VITE_FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.VITE_FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     }),
   });
 }
