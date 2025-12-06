a617d6eea23960380be96b6bdb6458862005505c
Yushan3952 <sow112021012@gmail.com>
2025-12-06 17:23:28 +0800

    Update points.js

--- api/points.js
+++ api/points.js
@@ -1,3 +1,15 @@
-export default function handler(req, res) {
-  res.status(200).json({ message: "points ok" });
+import { db } from "../firebase.js";
+import { collection, getDocs } from "firebase/firestore";
+
+export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  try {
+    const snap = await getDocs(collection(db, "points"));
+    const data = snap.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    res.status(200).json({ success: true, points: data });
+
+  } catch (err) {
+    res.status(500).json({ success: false, error: err.message });
+  }
 }
